04ec1ac84286c444b6e41843a62585b372632f36
tommy bountham <bvone@ymail.com>
2015-04-10 10:14:28 -0700

    add user input not a triangle for input test passw

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,51 @@
-var triangle = function(input) {
-  var total = input.reduce(function(a, b){
-    return a + b;
-  });
 
+var triangle = function(a, b, c) {
+
+var lengths = [Math.abs(a), Math.abs(b), Math.abs(c)],
+sorted = [],
+squares = [],
+i;
+
+sorted = lengths.sort(function(a, b) { return a-b});
+
+for (i = 0; i < sorted.length; i++) {
+squares[i] = Math.pow(sorted[i], 2);
 }
+
+if (sorted[0] + sorted[1] > sorted[2]) {
+
+if ((sorted[0] != sorted[1]) && (sorted[0] != sorted[2]) && (sorted[1] != sorted[2])
+&& (squares[0] + squares[1] != squares[2])) {
+return "scalene";
+}
+
+if ((sorted[0] === sorted[1]) && (sorted[0] != sorted[2])) {
+return "isosceles";
+}
+
+if ((sorted[0] === sorted[1]) && (sorted[0] === sorted[2]) && (sorted[1] === sorted[2])) {
+return "equilateral";
+}
+
+if (squares[0] + squares[1] === squares[2]) {
+return "correct";
+}
+
+} else {
+return "impossible";
+}
+
+};
+$(document).ready(function() {
+$('form#input').submit(function(event) {
+
+$('#type').empty();
+var a = $('input#side1').val();
+var b = $('input#side2').val();
+var c = $('input#side3').val();
+var result = triangle(a, b, c);
+$('#type').text(result);
+$('#result').show()
+event.preventDefault();
+});
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,29 @@
 describe('triangle', function() {
-    it("user input 1 and return 1", function() {
-        expect(triangle(1)).to.eql(1);
-    });
 
+it("user input : (3, 5, 5)", function() {
+expect(triangle(3, 5, 4)).to.equal("correct");
+});
 
+it("user input not a triangle for inputs: (4, 5, 10)", function() {
+expect(triangle(4, 5, 10)).to.equal("impossible");
+});
 
+it("user input a scalene triangle for inputs: (4, 5, 7)", function() {
+expect(triangle(4, 5, 7)).to.equal("scalene");
+});
 
+it("user input a isosceles triangle for inputs: (4, 4, 5)", function() {
+expect(triangle(4, 4, 5)).to.equal("isosceles");
+});
+
+it("user input a equilateral triangle for inputs: (3, 3, 3)", function() {
+expect(triangle(3, 3, 3)).to.equal("equilateral");
+});
+
+it("if user input negative values", function() {
+expect(triangle(4, 4, -4)).to.equal("equilateral");
+});
+it("user input a correct triangle (3, 5, 4)", function() {
+expect(triangle(3, 5, 4)).to.equal("correct");
+});
 });
