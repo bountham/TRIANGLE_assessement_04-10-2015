78df3ab882ee928bd0df756d055ddf70b67116df
tommy bountham <bvone@ymail.com>
2015-04-10 10:48:43 -0700

    create index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,51 @@
 
 var triangle = function(a, b, c) {
 
-var lengths = [Math.abs(a), Math.abs(b), Math.abs(c)],
-sorted = [],
-squares = [],
-i;
+    var length_number = [Math.abs(a), Math.abs(b), Math.abs(c)],
+    sort = [],
+    squaRes = [],
+    i;
 
-sorted = lengths.sort(function(a, b) { return a-b});
+  sort = length_number.sort(function(a, b) { return a-b});
 
-for (i = 0; i < sorted.length; i++) {
-squares[i] = Math.pow(sorted[i], 2);
-}
+  for (i = 0; i < sort.length; i++) {
+  squaRes[i] = Math.pow(sort[i], 2);
+  }
 
-if (sorted[0] + sorted[1] > sorted[2]) {
+  if (sort[0] + sort[1] > sort[2]) {
 
-if ((sorted[0] != sorted[1]) && (sorted[0] != sorted[2]) && (sorted[1] != sorted[2])
-&& (squares[0] + squares[1] != squares[2])) {
-return "scalene";
-}
+  if ((sort[0] != sort[1]) && (sort[0] != sort[2]) && (sort[1] != sort[2])
+  && (squaRes[0] + squaRes[1] != squaRes[2])) {
+  return "scalene";
+  }
 
-if ((sorted[0] === sorted[1]) && (sorted[0] != sorted[2])) {
-return "isosceles";
-}
+  if ((sort[0] === sort[1]) && (sort[0] != sort[2])) {
+  return "isosceles";
+  }
 
-if ((sorted[0] === sorted[1]) && (sorted[0] === sorted[2]) && (sorted[1] === sorted[2])) {
-return "equilateral";
-}
+  if ((sort[0] === sort[1]) && (sort[0] === sort[2]) && (sort[1] === sort[2])) {
+  return "equilateral";
+  }
 
-if (squares[0] + squares[1] === squares[2]) {
-return "correct";
-}
+  if (squaRes[0] + squaRes[1] === squaRes[2]) {
+  return "correct";
+  }
 
-} else {
-return "impossible";
-}
+  } else {
+  return "not-correct";
+  }
 
 };
-$(document).ready(function() {
-$('form#input').submit(function(event) {
+  $(document).ready(function() {
+  $('form#triangle').submit(function(event) {
 
-$('#type').empty();
-var a = $('input#side1').val();
-var b = $('input#side2').val();
-var c = $('input#side3').val();
-var result = triangle(a, b, c);
-$('#type').text(result);
-$('#result').show()
-event.preventDefault();
-});
+    var a = $('input#numberA').val();
+    var b = $('input#numberB').val();
+    var c = $('input#numberC').val();
+    var input_result = triangle(a, b, c);
+
+    $('#type').empt().text(input_result);
+    $('#result').show()
+    event.preventDefault();
+  });
 });
